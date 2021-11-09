8e3b8dd146c979da949910bd8ada2eeaecef1803
Greg Christiansen <gregchristiansen14@gmail.com>
2021-11-09 17:47:20 -0600

    added the timeTracker function to loop over the time blocks

--- assets/scipt.js
+++ assets/scipt.js
@@ -13,5 +13,30 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     });
 
+    function timeTracker() {
+        var timeNow = moment().hour();
+
+        // loop over the time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).att("id").split("hour")[1]);
+
+            if(blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if(blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
     
 })
