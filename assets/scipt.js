61e57ac1ad72069a62c7bf3e790bd9effed0ed13
Greg Christiansen <gregchristiansen14@gmail.com>
2021-11-09 17:36:15 -0600

    inserted click listener and local storage

--- assets/scipt.js
+++ assets/scipt.js
@@ -2,3 +2,16 @@
 var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").html(todayDate);
 
+$(document).ready(function () {
+    // insert click listener for saveBtn
+    $(".savBtn").on("click", function () {
+        // show the closests values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // use localStorage to save the text 
+        localStorage.setItem(time, text);
+    });
+
+    
+})
